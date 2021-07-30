045511029f6bd3d025b4cd26d3f10adcdd21216a
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-07-29 17:54:32 -0700

    top 3 differences analysis

--- code/pt_mnist.py
+++ code/pt_mnist.py
@@ -122,5 +122,6 @@ def mnist_pt_objective(config):
 
 
 if __name__ == "__main__":
-    test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'dropout': 0.5, 'adam_epsilon': 1e-7}
+    # test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'dropout': 0.5, 'adam_epsilon': 1e-7}
+    test_config = {'batch_size': 800, 'learning_rate': 0.0001955, 'epochs': 15, 'dropout': 0.8869, 'adam_epsilon': 0.1182}
     res = mnist_pt_objective(test_config)
